4bffa8ac579c5b98d4115a284d262269f9478852
Nasim <mdnasimhosen333@gmail.com>
2025-04-24 00:24:53 +0600

    Implement code changes to enhance functionality and improve performance

--- compare_func.c
+++ compare_func.c
@@ -4,5 +4,18 @@ int main()
 {
     char a[101], b[101];
     scanf("%s %s", a, b);
+    int val = strcmp(a, b);
+    if (val == 0)
+    {
+        printf("Equal\n");
+    }
+    else if (val < 0)
+    {
+        printf("a is smaller");
+    }
+    else if (val > 0)
+    {
+        printf("b is smaller");
+    }
     return 0;
 }
