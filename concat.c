7a035505e591dcb82de1716d3663dd6ecb4eadcf
Nasim <mdnasimhosen333@gmail.com>
2025-04-23 23:32:45 +0600

    learn concat

--- concat.c
+++ concat.c
@@ -1,6 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 
-int main(){
-    
+int main()
+{
+    char a[101], b[101];
+    scanf("%s %s", a, b);
+    int a_len = strlen(a);
+    int b_len = strlen(b);
+    for (int i = 0; i <= b_len; i++)
+    {
+        a[a_len + i] = b[i];
+    }
+    printf("%s", a);
     return 0;
 }
